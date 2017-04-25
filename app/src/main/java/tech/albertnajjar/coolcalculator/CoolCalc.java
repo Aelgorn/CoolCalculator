99dfc4d54df1461bba5b8d7b2c4309a5227e4df4
Albert Najjar <albertnajjar@outlook.com>
2017-04-25 17:13:19 -0400

    First commit

---
+++ app/src/main/java/tech/albertnajjar/coolcalculator/CoolCalc.java
@@ -0,0 +1,13 @@
+package tech.albertnajjar.coolcalculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CoolCalc extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cool_calc);
+    }
+}
